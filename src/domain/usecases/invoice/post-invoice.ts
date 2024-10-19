b0a6f172d840c2d372c4f82ed7902d47c5490da7
Rubens Engagers <rubens.felipe@hotmail.com>
2024-10-19 19:39:25 -0300

    fix: vercel deploy

--- src/domain/usecases/invoice/post-invoice.ts
+++ src/domain/usecases/invoice/post-invoice.ts
@@ -1,8 +1,7 @@
 import { Invoice } from '../../../data/data-store/models'
-import { InvoiceUpload } from '../../models/invoice-upload.model'
-
+import { InvoicePost } from '../../models/invoice-upload.model'
 export interface PostInvoice {
-  post: (body: InvoiceUpload | FormData) => Promise<InvoiceResponse>
+  post: (body: InvoicePost | FormData) => Promise<InvoiceResponse>
 }
 
 export type InvoiceResponse = Invoice | any
